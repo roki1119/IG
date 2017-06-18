d60b4266effaddd53b9d07a1348c53fd73da1026
Roki <m.roki93@gmail.com>
2017-06-18 19:46:05 +0200

    Finally Version

--- src/main/java/LauncherScript.bat
+++ src/main/java/LauncherScript.bat
@@ -3,4 +3,5 @@ javac Main.java
 @echo Hi, this is a program which reversing a sentences.
 java Main
 @echo Both sentences are working correct.
-pause
+@echo Please press any button to close the window.
+pause >nul
