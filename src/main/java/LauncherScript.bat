edf5622ed26a23a41e0e4f4497a4238d06517d35
Roki <m.roki93@gmail.com>
2017-06-18 20:06:05 +0200

    Finally Version

--- src/main/java/LauncherScript.bat
+++ src/main/java/LauncherScript.bat
@@ -1,7 +1,18 @@
 @echo off
+cls
+:start
 javac Main.java
+
 @echo Hi, this is a program which reversing a sentences.
 java Main
+
 @echo Both sentences are working correct.
-@echo Please press any button to close the window.
-pause >nul
+
+@echo You can write your sentence again.
+set choice=
+set /p choice="Do you want to restart? Press 'Y' button and enter for accept: "
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='Y' goto start
+
+@echo Please press any button to close the program.
+pause>nul
